ccfaa0d24ecfdd9be096a51815cf8c94ac8b659b
awehook <awehook@163.com>
2020-03-27 19:33:24 +0800

    feat: make this repo's language typescript

---
+++ src/main/index.ts
@@ -0,0 +1,16 @@
+import { app } from 'electron';
+import './ipc';
+import { App } from './app';
+import { Cli } from "./cli";
+const log = require('debug')('main:index');
+
+// log('env:', process.env);
+const gotTheLock = app.requestSingleInstanceLock();
+if (!gotTheLock) {
+    app.exit();
+}
+
+const cli = new Cli();
+
+const blinkApp = new App(cli);
+blinkApp.init();
